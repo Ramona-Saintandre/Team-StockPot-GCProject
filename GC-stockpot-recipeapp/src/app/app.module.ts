4a370328128a38f2d831d57309db6bc2635440d5
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-03 20:32:38 -0500

    recipe list component created

--- GC-stockpot-recipeapp/src/app/app.module.ts
+++ GC-stockpot-recipeapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchCriteriaComponent
+    SearchCriteriaComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
