abc20d61473a3672fb11592f86ae4c8ec7d73a0a
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-03 21:11:58 -0500

    creating header component, unable to serve

---
+++ GC-stockpot-recipeapp/src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core'
+import { SelectorContext } from '@angular/compiler';
+@Component ({
+    selector:'app-header',
+    templateUrl:'/header.component.html'
+
+})
+export class Header{
+
+}
