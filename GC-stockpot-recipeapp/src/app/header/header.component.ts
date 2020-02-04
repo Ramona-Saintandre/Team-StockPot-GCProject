a58601e63bf52def50f73d09b172e14e099ffaca
mobilerast <mobilerast@gmail.com>
2020-02-04 20:33:57 +0300

    fix build problem on repo

--- GC-stockpot-recipeapp/src/app/header/header.component.ts
+++ GC-stockpot-recipeapp/src/app/header/header.component.ts
@@ -1,9 +1,7 @@
 import { Component} from '@angular/core'
-import { SelectorContext } from '@angular/compiler';
 @Component ({
     selector:'app-header',
-    templateUrl:'/header.component.html'
-
+    templateUrl:'header.component.html'
 })
 export class Header{
 
