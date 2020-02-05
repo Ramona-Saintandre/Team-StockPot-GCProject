f4aed9a41228a8fa16f4381da48e3b4adf2255a9
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-05 01:18:57 -0500

    styled recipes-list component template

--- GC-stockpot-recipeapp/src/app/recipes/recipes-list/recipes-list.component.ts
+++ GC-stockpot-recipeapp/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes.model'
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-recipes = [];
+recipes: Recipe[] = [
+  new Recipe('a test recipe','This is to test the list','https://www.flickr.com/photos/67238971@N04/7375181062','crockpot')
+];
   constructor() { }
 
   ngOnInit(): void {
